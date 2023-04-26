00f797a5d181110b81566c7d958496f781262dfc
Jhester11 <102759486+Jhester11@users.noreply.github.com>
2023-04-27 00:50:18 +0800

    First Commit

---
+++ indexmica.js
@@ -0,0 +1,128 @@
+const bar = document.querySelector(".fa-bars");
+const menu = document.querySelector(".menu");
+
+bar.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+});
+
+
+const product = [
+  {
+      id: 0,
+      image: 'image/Doppio.jpg',
+      title: 'Doppio Coffee',
+      price: 140,
+  },
+  {
+      id: 1,
+      image: 'image/Espresso con panna.jpg',
+      title: 'Espresso con panna',
+      price: 165,
+  },
+  {
+      id: 2,
+      image: 'image/Americano.jpg',
+      title: 'Americano Coffee',
+      price: 170,
+  },
+  {
+      id: 3,
+      image: 'image/Macchiato.jpg',
+      title: 'Macchiato',
+      price: 160,
+  },
+  {
+      id: 4,
+      image: 'image/icecoffee.jpg',
+      title: 'Ice Coffee',
+      price: 170,
+  },
+  {
+      id: 5,
+      image: 'image/Flat white Espresso.jpg',
+      title: 'Flat white Espresso',
+      price: 180,
+  },
+  {
+      id: 6,
+      image: 'image/Cappuccino.jpg',
+      title: 'Cappuccino',
+      price: 150,
+  },
+   {
+      id: 7,
+      image: 'image/Latte.jpg',
+      title: 'Latte',
+      price: 140,
+  },
+  {
+    id: 8,
+      image: 'image/Mocha Frappe.jpg',
+      title: 'Mocha Frappe',
+      price: 140,
+  },
+  {
+    id: 9,
+      image: 'image/Affogato Coffee.jpg',
+      title: 'Affogato Coffee',
+      price: 140,
+  },
+];
+const categories = [...new Set(product.map((item)=>
+  {return item}))]
+  let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+  var {image, title, price} = item;
+  return(
+      `<div class='box'>
+          <div class='img-box'>
+              <img class='images' src=${image}></img>
+          </div>
+      <div class='bottom'>
+      <p>${title}</p>
+      <h2>Php ${price}.00</h2>`+
+      "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+      `</div>
+      </div>`
+  )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+  cart.push({...categories[a]});
+  displaycart();
+}
+function delElement(a){
+  cart.splice(a, 1);
+  displaycart();
+}
+
+function displaycart(){
+  let j = 0, total=0;
+  document.getElementById("count").innerHTML=cart.length;
+  if(cart.length==0){
+      document.getElementById('cartItem').innerHTML = "Your cart is empty";
+      document.getElementById("total").innerHTML = "Php "+0+".00";
+  }
+  else{
+      document.getElementById("cartItem").innerHTML = cart.map((items)=>
+      {
+          var {image, title, price} = items;
+          total=total+price;
+          document.getElementById("total").innerHTML = "Php "+total+".00";
+          return(
+              `<div class='cart-item'>
+              <div class='row-img'>
+                  <img class='rowimg' src=${image}>
+              </div>
+              <p style='font-size:12px;'>${title}</p>
+              <h2 style='font-size: 15px;'>Php ${price}.00</h2>`+
+              "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+          );
+      }).join('');
+  }
+
+  
+}
